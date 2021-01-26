399cb6b5a7577a00b173473cba9348c1e74d1400
skylerseifert <skylerseifert@gmail.com>
2021-01-25 20:25:01 -0800

    Add new iframe and cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import * as esbuild from 'esbuild-wasm';
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
     const ref = useRef<any>();
-    const [input, setInput] = useState('console.log(1);');
-    const [code, setCode] = useState('');
+    const iframe = useRef<any>();
+    const [input, setInput] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -15,13 +15,16 @@ const App = () => {
             wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
         });
     };
-
     useEffect(() => {
         startService();
     }, []);
 
     const onClick = async () => {
-        if (!ref.current) return;
+        if (!ref.current) {
+            return;
+        }
+
+        iframe.current.srcdoc = html;
 
         const result = await ref.current.build({
             entryPoints: ['index.js'],
@@ -34,21 +37,49 @@ const App = () => {
             },
         });
 
-        // console.log(result);
-
-        setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(
+            result.outputFiles[0].text,
+            '*'
+        );
     };
+
+    const html = `
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try {
+                        eval(event.data);
+                    } catch (err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                        console.error(err);
+                    }
+                }, false);
+            </script>
+        </body>
+    </html>
+  `;
 
     return (
         <div>
             <textarea
+                cols={30}
+                rows={10}
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
             ></textarea>
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
+            <iframe
+                title="preview"
+                ref={iframe}
+                sandbox="allow-scripts"
+                srcDoc={html}
+            />
         </div>
     );
 };
