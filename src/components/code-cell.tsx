47e03699953ca699538433969c3474b85eee12aa
skylerseifert <skylerseifert@gmail.com>
2021-02-01 00:01:57 -0800

    Create buffer for cells and allowing to add cells

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -33,7 +33,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             <Resizable direction="vertical">
                 <div
                     style={{
-                        height: '100%',
+                        height: 'calc(100% - 10px)',
                         display: 'flex',
                         flexDirection: 'row',
                     }}
