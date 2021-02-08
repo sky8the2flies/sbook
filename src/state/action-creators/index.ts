39d6f7cfe2d6567ebadcf90bea0f10e6695635e0
skylerseifert <skylerseifert@gmail.com>
2021-02-07 22:10:19 -0800

    Add bundle reducer and code cleanup

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,5 +1,7 @@
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
 import {
+    Action,
     UpdateCellAction,
     DeleteCellAction,
     MoveCellAction,
@@ -7,6 +9,7 @@ import {
     Direction,
 } from '../actions';
 import { CellTypes } from '../cell';
+import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
     return {
@@ -45,5 +48,26 @@ export const insertCellAfter = (
             id,
             type: cellTypes,
         },
+    };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+    return async (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.BUNDLE_START,
+            payload: {
+                cellId,
+            },
+        });
+
+        const result = await bundle(input);
+
+        dispatch({
+            type: ActionType.BUNDLE_COMPLETE,
+            payload: {
+                cellId,
+                bundle: result,
+            },
+        });
     };
 };
